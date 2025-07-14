2f9b1ad5df8eb06cb1f88a0932827e5fea4a3efe
DavidNew-NOAA <134300700+DavidNew-NOAA@users.noreply.github.com>
2025-07-14 12:25:55 -0400

    Fail on jinja2 render exception (#22)
    This PR causes jcb to raise an a generic exception if a generic
    exception is raised by the jinja2 renderer. Currently it causes only a
    warning to be printed to standard out.
    
    I don't know how many times my testing has failed because the yaml I get
    rendered has simply "null" in it. I would rather catch the failure at
    the rendering stage rather than at the point of running a JEDI
    application with a bad yaml.

--- src/jcb/renderer.py
+++ src/jcb/renderer.py
@@ -195,9 +195,10 @@ class Renderer():
         # Render the template hierarchy
         try:
             jedi_dict_yaml = template.render(self.template_dict)
-        except j2.exceptions.UndefinedError as e:
-            print(f'Resolving templates for {algorithm} failed with the following exception: {e}')
-            return None
+        except Exception as e:
+            msg = f'Resolving templates for {algorithm} failed with the following exception: {e}'
+            print(msg)
+            raise Exception(msg)
 
         # Check that everything was rendered
         jcb.abort_if('{{' in jedi_dict_yaml, f'In template_string_jinja2 '
